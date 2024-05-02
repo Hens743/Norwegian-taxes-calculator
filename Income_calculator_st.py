2e06269c0b13fa9a1dfb8ca4b074be0ba6af42dd
Hens743 <80003512+Hens743@users.noreply.github.com>
2024-05-02 14:20:52 +0200

    Update Income_calculator_st.py

--- Income_calculator_st.py
+++ Income_calculator_st.py
@@ -68,8 +68,8 @@ def main():
             # Visualization
             st.subheader("Tax breakdown visualization")
             fig = go.Figure()
-            labels = [label for label, _ in tax_details] + ['Net Income']
-            values = [value for _, value in tax_details] + [net_income]
+            labels = [label for label, _ in df] + ['Net Income']
+            values = [value for _, value in df] + [net_income]
             colors = ['orange', 'red', 'yellow', 'green', 'purple', 'pink']  # Custom colors for each tax type
             
             for i, (label, value) in enumerate(zip(labels, values)):

eadff2d04c5a98d1b478558c3a1d0d911fff5034
Hens743 <80003512+Hens743@users.noreply.github.com>
2024-05-28 15:24:44 +0200

    Update Income_calculator_st.py

--- Income_calculator_st.py
+++ Income_calculator_st.py
@@ -98,7 +98,7 @@ def main():
                 mime='text/csv'
             )
 
-            st.markdown(""" Note:  The taxes are calculated based on the tables of Norway, income tax. For simplification purposes some variables (such as marital status, place of living and others) have been assumed. This app does not represent legal authority and shall be used for approximation purposes only.
+            st.markdown(""" Note:  The taxes are calculated based on the tables of Norway income tax. For simplification purposes some variables (such as marital status, place of living and others) have been assumed. This app does not represent legal authority and shall be used for approximation purposes only.
  """) 
 
 if __name__ == "__main__":
